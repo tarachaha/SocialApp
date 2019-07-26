aa1bcb4f44357ca3a7cb21b45d58f55a13f67bff
Pawel Taracha <pawel.taracha@gmail.com>
2019-07-26 21:41:01 +0100

    Client side login and register

--- SocialApp.API/Controllers/ValuesController.cs
+++ SocialApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace SocialApp.API.Controllers
             _context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
